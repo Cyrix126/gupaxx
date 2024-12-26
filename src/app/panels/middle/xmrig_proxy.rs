78bd40e5ec59d0de2827d03c71a481b2dc915fee
Cyrix126 <58007246+Cyrix126@users.noreply.github.com>
2024-12-26 10:06:07 +0100

    fix: proxy tab wrong name for rig id

--- src/app/panels/middle/xmrig_proxy.rs
+++ src/app/panels/middle/xmrig_proxy.rs
@@ -193,7 +193,7 @@ impl XmrigProxy {
     }
     fn rig_field(&mut self, ui: &mut Ui) -> bool {
         StateTextEdit::new(ui)
-            .description("   Name     ")
+            .description(" RIG ID   ")
             .max_ch(30)
             .help_msg(XMRIG_RIG)
             .build(ui, &mut self.rig)
