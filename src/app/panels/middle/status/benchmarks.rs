a6ac34594bc4093123b96caf9a06489b880c88a7
Cyrix126 <gupaxx@baermail.fr>
2024-12-27 18:23:47 +0100

    fix: add missing license notice and fix author

--- src/app/panels/middle/status/benchmarks.rs
+++ src/app/panels/middle/status/benchmarks.rs
@@ -1,3 +1,20 @@
+// Gupaxx - Fork of Gupax
+//
+// Copyright (c) 2024-2025 Cyrix126
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 use std::sync::{Arc, Mutex};
 
 use crate::{app::Benchmark, disk::state::Status, helper::xrig::xmrig::PubXmrigApi};
